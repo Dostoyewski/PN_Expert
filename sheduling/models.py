18eb0270886a9ae909843abf47cca3b8032b800e
Dostoyewski <dostoyewski@yandex.ru>
2021-05-06 17:16:46 +0300

    Updated sheduling process #3

--- sheduling/models.py
+++ sheduling/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib.auth.models import User
 from django.db import models
 from django_q.models import Schedule
@@ -13,7 +15,7 @@ SHEDULE_TYPE = (
 )
 
 
-def create_events(users):
+def create_events(pk):
     print("asdasd")
 
 
@@ -35,22 +37,16 @@ class SurveyShedule(models.Model):
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
-        users = self.users.all()
-        # if self.run_interval == 0:
-        #     schedule(create_events, users,
-        #              schedule_type=Schedule.DAILY,
-        #              next_run=datetime.datetime.now() + datetime.timedelta(seconds=10))
-        # elif self.run_interval == 1:
-        #     schedule(create_events, users,
-        #              schedule_type=Schedule.WEEKLY)
-        # elif self.run_interval == 2:
-        #     schedule(create_events, users,
-        #              schedule_type=Schedule.MONTHLY)
-        # elif self.run_interval == 3:
-        #     schedule(create_events, users,
-        #              schedule_type=Schedule.QUARTERLY)
-        schedule('math.sqrt',
-                 9,
-                 hook='hooks.print_result',
-                 q_options={'timeout': 30, 'broker_name': 'broker_1'},
-                 schedule_type=Schedule.HOURLY)
+        if self.run_interval == 0:
+            schedule('create_events', self.pk,
+                     schedule_type=Schedule.DAILY,
+                     next_run=datetime.datetime.now() + datetime.timedelta(seconds=10))
+        elif self.run_interval == 1:
+            schedule('create_events', self.pk,
+                     schedule_type=Schedule.WEEKLY)
+        elif self.run_interval == 2:
+            schedule('create_events', self.pk,
+                     schedule_type=Schedule.MONTHLY)
+        elif self.run_interval == 3:
+            schedule('create_events', self.pk,
+                     schedule_type=Schedule.QUARTERLY)
