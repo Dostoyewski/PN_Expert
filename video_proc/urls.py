94001b32a1b8ff55b73cb82eb278d8c4dcedc098
Dostoyewski <dostoyewski@yandex.ru>
2021-05-10 23:36:21 +0300

    Fixed run error

--- video_proc/urls.py
+++ video_proc/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.competitions_list, name='competition_list'),
     path('<slug:slug>', views.video_recording, name='video_rec'),
     path('api/competition/list/', views.competition_list, name='compet_list'),
     path('api/recording/create/', views.create_recording, name='create_rec'),
