a5afa9ff0fa20a7849b359d282637f1396297e5d
Dostoyewski <dostoyewski@yandex.ru>
2021-05-10 23:33:53 +0300

    Added video API outs #3

--- video_proc/serializers.py
+++ video_proc/serializers.py
@@ -7,29 +7,13 @@ class CompetitionRecordingSerializer(serializers.Serializer):
     """
     CompetitionRecording serializer
     """
-    published = serializers.DateTimeField()
 
-    def create(self, validated_data):
-        return CompetitionRecording.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        # instance.header = validated_data.get('header', instance.header)
-        instance.save()
-        return instance
+    class Meta:
+        model = CompetitionRecording
+        exclude = ''
 
 
-class CompetitionSerializer(serializers.Serializer):
-    header = serializers.CharField(max_length=50)
-    text = serializers.CharField(max_length=2000)
-    video_ref = serializers.CharField()
-    exp = serializers.IntegerField()
-    slug = serializers.SlugField()
-
-    def create(self, validated_data):
-        return Competition.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.header = validated_data.get('header', instance.header)
-        instance.text = validated_data.get('text', instance.text)
-        instance.save()
-        return instance
+class CompetitionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Competition
+        fields = '__all__'
--- video_proc/urls.py
+++ video_proc/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.competitions_list, name='competition_list'),
     path('<slug:slug>', views.video_recording, name='video_rec'),
-    path('upload/', views.grishas_intro, name='intro'),
-    path('upload_timer/', views.grishas_intro_timer, name='intro_timer')
+    path('api/competition/list/', views.competition_list, name='compet_list'),
+    path('api/recording/create/', views.create_recording, name='create_rec'),
 ]
