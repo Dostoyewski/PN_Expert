3f455fce2287f6e5c11909902a9f2d7619079715
Dostoyewski <dostoyewski@yandex.ru>
2021-07-18 17:52:13 +0300

    Added time delay

--- sheduling/views.py
+++ sheduling/views.py
@@ -1,3 +1,5 @@
+import time
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -6,10 +8,14 @@ from .models import SurveyShedule, MessageSurvey
 
 @api_view(['POST'])
 def reload_all_shedules(request):
+    print("start reloading")
     ss_o = SurveyShedule.objects.all()
     for obj in ss_o:
+        time.sleep(0.2)
         obj.save()
     ss_o = MessageSurvey.objects.all()
     for obj in ss_o:
+        time.sleep(0.2)
         obj.save()
+    print("reloading completed")
     return Response({"message": "reloaded"})
