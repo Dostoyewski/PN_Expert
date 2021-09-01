6300784560e01c4c8bb02501bf9aa7544a600a72
Dostoyewski <dostoyewski@yandex.ru>
2021-09-01 15:23:53 +0300

    Remove negative indexing

--- survey/models.py
+++ survey/models.py
@@ -103,7 +103,7 @@ class SurveyAnswer(models.Model):
         elif 'роль' in self.survey.title:
             up = UserProfile.objects.get(user=self.user)
             up.isStatus = True
-            answer = self.answers.all()[-1]
+            answer = self.answers.last()
             print(answer.answer)
             if answer.answer == 'Врач':
                 up.status = 1
