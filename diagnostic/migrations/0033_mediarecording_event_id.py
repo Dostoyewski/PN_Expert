71a2d95c139f44fd518a45284d3ddc25c17f2e8e
Fedor Kondratenko <dostoyewski@yandex.ru>
2021-12-14 20:54:14 +0300

    fix migrations

--- diagnostic/migrations/0033_mediarecording_event_id.py
+++ diagnostic/migrations/0033_mediarecording_event_id.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='mediarecording',
             name='event_id',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='diagnostic.event'),
+            field=models.ForeignKey(default=1000, on_delete=django.db.models.deletion.CASCADE, to='diagnostic.event'),
         ),
     ]
