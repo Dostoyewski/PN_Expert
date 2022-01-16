21f9d5342a10884768c798b66e5b4ec3a5448ada
Dostoyewski <dostoyewski@yandex.ru>
2022-01-16 13:55:50 +0300

    Add user statistics methods

--- lk/admin.py
+++ lk/admin.py
@@ -1,5 +1,7 @@
+import pandas as pd
 from django.contrib import admin
 
+from diagnostic.models import Event, DoctorEvent
 from lk.models import create_events
 from sheduling.models import MessageSurvey, SurveyShedule
 from .models import UserProfile, NewsRecording, DiaryRecording, StepsCounter, HADS_Result, HADS_Alarm, \
@@ -20,6 +22,31 @@ def reload_all(modeladmin, request, queryset):
         reload_all_events(obj)
 
 
+@admin.action(description='Выгрузить статистику')
+def create_statistics(modeladmin, request, queryset):
+    df = pd.DataFrame(columns=['User', 'status', 'All events', 'Done events'])
+    for obj in queryset:
+        df = df.append(create_user_stats(obj), ignore_index=True)
+    df.to_excel('./UserStats.xlsx')
+
+
+def create_user_stats(user_profile):
+    events, done_events = None, None
+    status = ""
+    if user_profile.status == 0:
+        events = Event.objects.filter(user=user_profile.user)
+        done_events = Event.objects.filter(user=user_profile.user, isDone=True)
+        status = "Пациент"
+    elif user_profile.status == 1:
+        events = DoctorEvent.objects.filter(user=user_profile.user)
+        done_events = DoctorEvent.objects.filter(user=user_profile.user, isDone=True)
+        status = "Врач"
+    return {'User': user_profile.user.username,
+            'status': status,
+            'All events': len(events),
+            'Done events': len(done_events)}
+
+
 def reload_all_events(user_profile):
     surveys = SurveyShedule.objects.filter(users=user_profile.user)
     m_surveys = MessageSurvey.objects.filter(users=user_profile.user)
@@ -38,7 +65,7 @@ class UserProfileAdmin(admin.ModelAdmin):
                     'gender', 'age', 'status', 'exp', 'isFull', 'slug')
     list_filter = ('user', 'name', 'gender', 'status', 'doctor')
     search_fields = ['user', 'doctor__user__username', 'doctor__user', 'doctor_name', 'name']
-    actions = [reload_all]
+    actions = [reload_all, create_statistics]
 
 
 class NewsAdmin(admin.ModelAdmin):
